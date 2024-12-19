ec97c05b257b93492e3c91ecbfa64613638e7cba
Louis Horrell <lhorrell99@gmail.com>
2024-12-19 23:09:45 +0000

    Agressively over-engineered 12-01 part 1

--- 12-01.py
+++ 12-01.py
@@ -1,24 +1,26 @@
+from typing import List, Tuple
+from utils import *
+
+
+def get_differences(data: List[Tuple[int, int]]) -> List[int]:
+    return [abs(a - b) for a, b in data]
+
+
 filepath = "./data/12-01.txt"
-
-
-def load_data(filepath: str, split_delimiter: str):
-    with open(filepath) as file:
-        data = file.read()
-
-    return data.split(split_delimiter)
-
 
 # Load data
 data = load_data(filepath, "\n")
 
-# Split values
-data = [i.split() for i in data]
+# Compose a solver
+solve = compose(
+    split_strings,
+    transpose,
+    sort_sublists,
+    parse_to_int,
+    zip_sublists,
+    get_differences,
+    sum,
+)
 
-# Transpose and sort
-a_list, b_list = [sorted(list(i)) for i in zip(*data)]
-
-# Find differences
-differences = [abs(int(i[0]) - int(i[1])) for i in zip(a_list, b_list)]
-
-# Print sum
-print(sum(differences))
+# Print result
+print(solve(data))
---
+++ utils.py
@@ -0,0 +1,33 @@
+from typing import List
+import functools
+
+
+def compose(*funcs):
+    return lambda x: functools.reduce(lambda acc, f: f(acc), funcs, x)
+
+
+def load_data(filepath: str, split_delimiter: str) -> list:
+    with open(filepath) as file:
+        data = file.read()
+
+    return data.split(split_delimiter)
+
+
+def parse_to_int(data: List[List[str]]) -> List[List[int]]:
+    return [[int(j) for j in i] for i in data]
+
+
+def transpose(data: List[list]) -> List[list]:
+    return [list(i) for i in zip(*data)]
+
+
+def split_strings(data: List[str], split_delimiter: str | None = None) -> list:
+    return [i.split(split_delimiter) for i in data]
+
+
+def sort_sublists(data: List[list]) -> List[list]:
+    return [sorted(i) for i in data]
+
+
+def zip_sublists(data: List[list]) -> List[list]:
+    return list(zip(*data))
