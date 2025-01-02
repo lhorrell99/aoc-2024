edf3bded1e113dc06eec698b6151518d0ee1aa58
Louis Horrell <lhorrell99@gmail.com>
2025-01-02 11:21:47 +0000

    12-04 part 2 (ugly)

--- 04-p2.py
+++ 04-p2.py
@@ -2,61 +2,57 @@ from utils import *
 import re
 
 # Filepath
-filepath = "./data/12-04.txt"
+filepath = "./data/12-04-test.txt"
 
 
 # Problem-specific functions
 get_row_len = compose(transpose, len)
 
 
+def test_pair(str_char: str, search_char: str, skip_char: str = ".") -> bool:
+    return True if str_char == search_char or search_char == skip_char else False
+
+
 def find_all_in_string(string: str, s_pattern: str, skip_char: str = ".") -> int:
-    
-    
-    pass
+    pairwise = [
+        list(zip(string[i:], s_pattern))
+        for i in range(len(string) - (len(s_pattern) - 1))
+    ]
+    matches = [all(test_pair(*i) for i in j) for j in pairwise]
+    return sum(matches)
 
 
 def count_in_matrix(data: List[str], s_pattern: List[str], skip_char: str = ".") -> int:
     # Pad input
-    padded_data = pad_rows(data, ".")
+    padded_data = pad_rows(data)
 
-    # Get row length
+    # Get row lengths
     row_len = get_row_len(padded_data)
     search_row_len = get_row_len(s_pattern)
 
     # Flatten
     flattened_data = flatten(padded_data)
 
-    # Build 2D search pattern
+    # Build flattened search pattern
     s_pattern = f"{skip_char * (row_len - search_row_len)}".join(s_pattern)
 
-    print(s_pattern)
-    # # Build search regex
-    # search = re.compile("")
+    return find_all_in_string(flattened_data, s_pattern, ".")
 
 
-print(get_row_len(["xox", "xox"]))
-
-print(count_in_matrix(["xox", "xox", "xox"], ["xox", "xox"]))
-# def skew(data: List[str], s_char: str = "*") -> List[str]:
-#     return [s_char * i + s + s_char * (len(data) - (i + 1)) for i, s in enumerate(data)]
+# Load data
+data = load_data(filepath, "\n")
 
 
-# count_all_in_sublists = compose(pad_rows, flatten, lambda x: x.count("XMAS"))
+# Compose a solver
+solve = compose(
+    array_apply(
+        lambda x: x,
+        transpose,
+        flip_cols,
+        compose(transpose, flip_cols),
+    ),
+    lambda x: [count_in_matrix(i, ["M.S", ".A.", "M.S"]) for i in x],
+    sum,
+)
 
-
-# # Load data
-# data = load_data(filepath, "\n")
-
-# # Compose a solver
-# solve = compose(
-#     array_apply(
-#         lambda x: x,
-#         transpose,
-#         flip_cols,
-#         compose(transpose, flip_cols),
-#     ),
-#     lambda x: [count_all_in_sublists(i) for i in x],
-#     sum,
-# )
-
-# print(solve(data))
+print(solve(data))
